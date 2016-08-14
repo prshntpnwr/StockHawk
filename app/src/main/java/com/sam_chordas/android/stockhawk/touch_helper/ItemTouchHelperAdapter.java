afe4cf7345fd428362145ebdd31345f9076cd793
Prashant Panwar <prashant.panwar777@gmail.com>
2016-08-14 21:47:00 +0530

    feat:Empty view if no network available, refactor:2 space indention to 4 Space indention

--- app/src/main/java/com/sam_chordas/android/stockhawk/touch_helper/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/touch_helper/ItemTouchHelperAdapter.java
@@ -3,11 +3,9 @@ package com.sam_chordas.android.stockhawk.touch_helper;
 import android.view.View;
 
 /**
- * Created by sam_chordas on 10/6/15.
- * credit to Paul Burke (ipaulpro)
  * Interface to enable swipe to delete
  */
 public interface ItemTouchHelperAdapter {
 
-  void onItemDismiss(int position);
+    void onItemDismiss(int position);
 }
